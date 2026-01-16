39c2ca784e03079778d2ca6196416f527a5fbcd8
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-01-16 19:04:29 +0300

    feat: add script for the dish_ingredient table creation

---
+++ src/main/resources/sql/new_schema.sql
@@ -0,0 +1,9 @@
+create type unit_type as enum('PCS', 'KG', 'L');
+
+create table dish_ingredient(
+    id serial primary key,
+    id_dish int references dish (id),
+    id_ingredient int references ingredient (id),
+    quantity_required numeric,
+    unit unit_type
+);
