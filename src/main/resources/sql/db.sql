ed89c5761eb3dbc82413c46db79b1463394b84d2
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-01-16 18:58:58 +0300

    feat: add grant privileges script to mini_dish_db_manager

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -3,3 +3,12 @@ create database "mini_dish_db";
 create user "mini_dish_db_manager" with password '123456';
 
 -- Grant all privileges
+GRANT CONNECT ON DATABASE mini_dish_db TO mini_dish_db_manager;
+
+--Authorize CRUD operations on all future tables
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO mini_dish_db_manager ;
+
+--Auto-increment usage
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO mini_dish_db_manager ;
