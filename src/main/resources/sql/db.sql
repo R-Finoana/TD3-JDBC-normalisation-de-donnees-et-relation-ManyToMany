f46f7769fca47396f41b1090df78d64a1cbb4185
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-01-18 15:34:45 +0300

    feat: add script to authorize tables creation to mini_dish_db_manager

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -5,6 +5,9 @@ create user "mini_dish_db_manager" with password '123456';
 -- Grant all privileges
 GRANT CONNECT ON DATABASE mini_dish_db TO mini_dish_db_manager;
 
+--Authorize tables creation
+    GRANT CREATE ON SCHEMA public TO mini_dish_db_manager;
+
 --Authorize CRUD operations on all future tables
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
     GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO mini_dish_db_manager ;
