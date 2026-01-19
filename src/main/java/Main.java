64541324ff1efeb263577850e4dd4b3a2d6c3965
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-01-19 14:54:35 +0300

    feat: add test for getGrossMargin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,10 +19,25 @@ public class Main {
         System.out.println("\n--FindDishById and getDishCost --");
         try {
             Dish salade = dataRetriever.findDishById(1);
-            System.out.println("Salade fraîche cost: " + salade.getDishCost());
+            System.out.println("Salade fraîche cost: " + salade.getDishCost());//should be 250
             System.out.println(salade);
         } catch (RuntimeException e) {
             System.out.println("Exception for Salade: " + e.getMessage());
+        }
+
+        System.out.println("\n--Gross margin test--");
+        try {
+            Dish poulet = dataRetriever.findDishById(2);
+            System.out.println("Poulet grillé margin: " + poulet.getGrossMargin());  //should be 7500
+        } catch (RuntimeException e) {
+            System.out.println("Exception for Poulet: " + e.getMessage());
+        }
+
+        try {
+            Dish riz = dataRetriever.findDishById(3);
+            System.out.println("Riz margin: " + riz.getGrossMargin());
+        } catch (RuntimeException e) {
+            System.out.println("Exception for Riz: " + e.getMessage());
         }
     }
 }
