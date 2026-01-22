5874206b0714cdfd6908b635c1e666aed5ef74f5
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-01-22 11:51:56 +0300

    fix: add quotation marks to the scripts in db.sql for accuracy

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -1,19 +1,19 @@
-create database mini_dish_db;
+create database "mini_dish_db";
 
-create user mini_dish_db_manager with password '123456';
+create user "mini_dish_db_manager" with password '123456';
 
 -- Grant all privileges
-GRANT CONNECT ON DATABASE mini_dish_db TO mini_dish_db_manager;
+GRANT CONNECT ON DATABASE "mini_dish_db" TO "mini_dish_db_manager";
 
 -- /c mini_dish_db
 
 --Authorize tables creation
-    GRANT CREATE ON SCHEMA public TO mini_dish_db_manager;
+    GRANT CREATE ON SCHEMA public TO "mini_dish_db_manager";
 
 --Authorize CRUD operations on all future tables
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
-    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO mini_dish_db_manager ;
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "mini_dish_db_manager" ;
 
 --Auto-increment usage
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
-    GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO mini_dish_db_manager ;
+    GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO "mini_dish_db_manager" ;
